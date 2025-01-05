0a6b5e9f14c34b4410cab837950b87d876cc0328
croeer <christian@roeer.info>
2025-01-05 15:59:06 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,64 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = var.api_name
+  protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST"]
+    allow_headers = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = var.integration_lambda_arn
+  payload_format_version = "2.0"
+}
+
+# resource "aws_apigatewayv2_authorizer" "jwt_authorizer" {
+#   api_id           = aws_apigatewayv2_api.http_api.id
+#   name             = "jwt-authorizer"
+#   authorizer_type  = "JWT"
+#   identity_sources = ["$request.header.Authorization"]
+#   jwt_configuration {
+#     issuer   = "https://idp.ku0.de/realms/plungestreak"
+#     audience = ["plungestreak"]
+#   }
+# }
+
+resource "aws_apigatewayv2_route" "default_get_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  # authorization_type = "JWT"
+  # authorizer_id = aws_apigatewayv2_authorizer.jwt_authorizer.id
+}
+
+resource "aws_apigatewayv2_route" "default_post_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  # authorization_type = "JWT"
+  # authorizer_id = aws_apigatewayv2_authorizer.jwt_authorizer.id
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_stage.default_stage.invoke_url
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "Allow${var.api_name}ApiGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.integration_lambda_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /* part allows invocation from any stage, method and resource path
+  # within API Gateway.
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*/{proxy+}"
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "api_name" {
+  type = string
+}
+
+variable "plungestreak_arn" {
+  type    = string
+  default = ""
+}
+
+variable "integration_lambda_name" {
+  type    = string
+  default = ""
+}
+
+variable "integration_lambda_arn" {
+  type    = string
+  default = ""
+}
